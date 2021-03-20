1dcc8b997911625f25faca3d517152c9fe67d63e
Adam Coscia <47363758+AdamCoscia@users.noreply.github.com>
2021-03-20 12:04:53 -0400

    Update isometric

--- sections/fun/games/isometric/main.js
+++ sections/fun/games/isometric/main.js
@@ -1,4 +1,44 @@
 /** GLOBALS */
+import { P3, axoProjMat } from "./draw.js";
+
+// function creates a 3D point (vertex)
+function vertex(x, y, z) {
+  return { x, y, z };
+}
+// an array of vertices
+const vertices = []; // an array of vertices
+
+// create the 8 vertices that make up a box
+const boxSize = 20; // size of the box
+const hs = boxSize / 2; // half size shorthand for easier typing
+vertices.push(vertex(-hs, -hs, -hs)); // lower top left  index 0
+vertices.push(vertex(hs, -hs, -hs)); // lower top right
+vertices.push(vertex(hs, hs, -hs)); // lower bottom right
+vertices.push(vertex(-hs, hs, -hs)); // lower bottom left
+vertices.push(vertex(-hs, -hs, hs)); // upper top left  index 4
+vertices.push(vertex(hs, -hs, hs)); // upper top right
+vertices.push(vertex(hs, hs, hs)); // upper bottom right
+vertices.push(vertex(-hs, hs, hs)); // upper  bottom left index 7
+
+const colours = {
+  dark: "#040",
+  shade: "#360",
+  light: "#ad0",
+  bright: "#ee0",
+};
+
+function createPoly(indexes, colour) {
+  return {
+    indexes,
+    colour,
+  };
+}
+
+const polygons = [];
+
+polygons.push(createPoly([1, 2, 6, 5], colours.shade)); // right face
+polygons.push(createPoly([2, 3, 7, 6], colours.light)); // front face
+polygons.push(createPoly([4, 5, 6, 7], colours.bright)); // top face
 
 class inputController {
   keyMap;
@@ -16,53 +56,53 @@ class inputController {
     this.keyMap = {
       w: {
         code: "KeyW",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > KeyW"),
+        down: () => console.log("keydown > KeyW"),
       },
       a: {
         code: "KeyA",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > KeyA"),
+        down: () => console.log("keydown > KeyA"),
       },
       s: {
         code: "KeyS",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > KeyS"),
+        down: () => console.log("keydown > KeyS"),
       },
       d: {
         code: "KeyD",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > KeyD"),
+        down: () => console.log("keydown > KeyD"),
       },
       p: {
         code: "KeyP",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > KeyP"),
+        down: () => console.log("keydown > KeyP"),
       },
       " ": {
         code: "Space",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > Space"),
+        down: () => console.log("keydown > Space"),
       },
       ArrowLeft: {
         code: "ArrowLeft",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > ArrowLeft"),
+        down: () => console.log("keydown > ArrowLeft"),
       },
       ArrowUp: {
         code: "ArrowUp",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > ArrowUp"),
+        down: () => console.log("keydown > ArrowUp"),
       },
       ArrowRight: {
         code: "ArrowRight",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > ArrowRight"),
+        down: () => console.log("keydown > ArrowRight"),
       },
       ArrowDown: {
         code: "ArrowDown",
-        up: () => console.log("keyup"),
-        down: () => console.log("keydown"),
+        up: () => console.log("keyup > ArrowDown"),
+        down: () => console.log("keydown > ArrowDown"),
       },
     };
     /**
@@ -110,7 +150,7 @@ class inputController {
       },
       false
     );
-    for (const [id, el] of Object.entries(this.clickMap)) {
+    for (const [_, el] of Object.entries(this.clickMap)) {
       el.ref.addEventListener("mousedown", el.press);
       el.ref.addEventListener("touchstart", el.press);
       el.ref.addEventListener("mouseup", el.depress);
@@ -174,8 +214,10 @@ function startGame() {
   // attach controls
   inputs.bind();
   inputs.attach();
+  // Set drawstyle
+  axoProjMat.setProjection("Isometric");
   // Set game timer.
-  gameTimer = setInterval(gameLoop, 1000 / 30);
+  gameTimer = setInterval(gameLoop, 1000 / 60);
 }
 
 function pauseGame() {
@@ -191,5 +233,29 @@ function endGame() {
 }
 
 function gameLoop() {
-  // TODO
+  var x, y, z;
+  for (z = 0; z < 4; z++) {
+    const hz = z / 2;
+    for (y = hz; y < 4 - hz; y++) {
+      for (x = hz; x < 4 - hz; x++) {
+        // move the box
+        const translated = vertices.map((vert) => {
+          return P3(vert.x + x * boxSize, vert.y + y * boxSize, vert.z + z * boxSize);
+        });
+
+        // create a new array of 2D projected verts
+        const projVerts = translated.map((vert) => axoProjMat.project(vert));
+        // and render
+        polygons.forEach((poly) => {
+          CTX.fillStyle = poly.colour;
+          CTX.strokeStyle = poly.colour;
+          CTX.lineWidth = 1;
+          CTX.beginPath();
+          poly.indexes.forEach((index) => CTX.lineTo(projVerts[index].x, projVerts[index].y));
+          CTX.stroke();
+          CTX.fill();
+        });
+      }
+    }
+  }
 }
