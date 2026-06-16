8aa408eb1467afc2ed89166f3bd0f3aae95b93dc
AdamCoscia <acoscia125@gmail.com>
2026-06-16 15:49:35 -0400

    update agentk demo

---
+++ papers/agentk/demo/assets/phase1-DHRcNGNZ.js
@@ -0,0 +1,1 @@
+import o from"./index-DVrXW2xx.js";import{c as e,o as a}from"./index-ilDF-e95.js";const n={__name:"phase1",setup(c){return(r,t)=>(a(),e(o,{condition:"phase1"}))}};export{n as default};
---
+++ papers/agentk/demo/assets/practice-C8rtdHYa.js
@@ -0,0 +1,1 @@
+import c from"./index-DVrXW2xx.js";import{c as o,o as r}from"./index-ilDF-e95.js";const p={__name:"practice",setup(t){return(e,a)=>(r(),o(c,{condition:"practice"}))}};export{p as default};
