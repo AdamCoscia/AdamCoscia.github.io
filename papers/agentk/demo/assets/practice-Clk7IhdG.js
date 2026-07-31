454af8a35236be2bcf4d27811d3e76b35bc04738
AdamCoscia <acoscia125@gmail.com>
2026-07-30 22:52:56 -0400

    update agentk demo

---
+++ papers/agentk/demo/assets/practice-Clk7IhdG.js
@@ -0,0 +1,1 @@
+import c from"./index-DZ9VreLP.js";import{c as o,o as r}from"./index-BAf0_U48.js";const p={__name:"practice",setup(t){return(e,a)=>(r(),o(c,{condition:"practice"}))}};export{p as default};
